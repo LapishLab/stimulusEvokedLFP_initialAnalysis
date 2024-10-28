d5dc67d2f2164f3f9b097e5ea3b7d99a46e44427
davidswygart <davidswygart@gmail.com>
2024-10-28 15:44:26 -0400

    Add error checking to stim times

--- plotIV.m
+++ plotIV.m
@@ -178,4 +178,25 @@ function stimTimes = getStimTimes(recording)
     processor = eventProcessors{1};
     events = recording.ttlEvents(processor);
     stimTimes = events.timestamp(events.state==1);
+    stimOff = events.timestamp(events.state==0);
+
+    if(length(stimTimes) ~= length(stimOff))
+        warning(['A different number of Event Onset and Offset times' ...
+            ' were recorded: Onsets=%i, Offsets=%i'] ...
+            , length(stimTimes), length(stimOff))
+    end
+
+    stimDiff = stimOff - stimTimes;
+
+    if(any(stimDiff<0))
+        warning('A stimulus Offset was recorded before a stimulus Onset')
+    end
+
+    % figure(123); clf; 
+    % scatter(stimTimes, ones(length(stimTimes)), '.g')
+    % hold on;
+    % scatter(stimOff, zeros(length(stimOff)), '.r')
+    % 
+    % figure(123); clf
+    % histogram(stimDiff)
 end
