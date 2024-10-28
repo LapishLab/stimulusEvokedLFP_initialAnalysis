ddc778ee6c5d4a37ccc65218cc612f12c8455824
davidswygart <davidswygart@gmail.com>
2024-10-28 18:50:01 -0400

    calculate half max activation

--- plotIV.m
+++ plotIV.m
@@ -34,10 +34,35 @@ peaks = getPeakResponse(shapedData);
 figure(4); clf
 plotPeaks(currents,peaks)
 
+figure(5); clf
+calcStimHalfMax(currents, peaks)
+
 % figure(5)
 % plotMovie(slicedData)
-
 %% Functions
+function calcStimHalfMax(currents,peaks)
+    p = median(peaks, 1);
+    p = [0,p];
+    currents = [0,currents];
+
+    [~, maxInd] = max(abs(p)); 
+    p = p / p(maxInd); 
+
+    sigModel = fit(currents',p', 'logistic');
+    halfMax = sigModel.c;
+
+    plot(currents,p, 'k')
+    hold on
+    plot(sigModel)
+    legend('Real','Fit', 'AutoUpdate','off','Location','west')
+
+    xline(halfMax, '--r')
+    ylim([-0.1,1.1])
+    yline(0)
+    yline(1)
+    text(halfMax,.1, ['Half-Max = ', num2str(halfMax)])
+end
+
 function plotMovie(slicedData)
 numTrials = size(slicedData,3);
 ymax = max(slicedData(:)) * .75;
