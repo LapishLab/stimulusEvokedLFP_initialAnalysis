cda18ebf8842d791454d7d73d3d0cb50114a9a2f
davidswygart <davidswygart@gmail.com>
2024-10-28 20:42:54 -0400

    Delete recFolder.m

--- recFolder.m
+++
@@ -1,7 +0,0 @@
-%rec_folder = 'C:\Users\david\Desktop\Plasticity\2024-10-23_12-54-19';
-%rec_folder = 'C:\Users\david\Desktop\Plasticity\Rat_51\2024-10-24_13-12-17';
-%rec_folder = 'C:\Users\david\Desktop\Plasticity\2024-10-28_11-28-25';
-% rec_folder = 'C:\Users\david\Desktop\Plasticity\Rat_53\2024-10-28_12-59-15';
-% rec_folder = 'C:\Users\david\Desktop\Plasticity\Rat_53\2024-10-28_13-46-51';
-% rec_folder = 'C:\Users\david\Desktop\Plasticity\2024-10-28_14-52-49';
-rec_folder = 'C:\Users\david\Desktop\Plasticity\2024-10-28_14-14-17';
