ea23f6f5d3939b1426e46d5cd8d60f7b9f878ff2
davidswygart <davidswygart@gmail.com>
2024-11-13 17:44:18 -0500

    handle multiple recordings or nodes

--- loadRecording.m
+++ loadRecording.m
@@ -1,7 +1,33 @@
 function recording = loadRecording(rec_path)
     session = Session(rec_path);
-    % Only look at the first record node for now (assume their will only be 1)
-    node = session.recordNodes{1};
-    % Only look at the first recording for now (assume their will only be 1)
-    recording = node.recordings{1,1};
+    
+    numNodes = length(session.recordNodes);
+    if numNodes>1
+        names = strings(numNodes,1);
+        for i=1:numNodes
+            names(i) = session.recordNodes{i}.name;
+        end
+        
+        fprintf('%s \n', strcat(string(1:numNodes)',":  ", names))
+        choice = input('Which node to analyze? Enter number from above:  ');
+        node = session.recordNodes{choice};
+    else
+        node = session.recordNodes{1};
+    end
+    
+
+    numRecs = length(node.recordings);
+    if numRecs>1
+        names = strings(numRecs,1);
+        for i=1:numRecs
+            names(i) = node.recordings{i}.directory;
+        end
+
+        fprintf('%s \n', strcat(string(1:numRecs)',":  ", names))
+        choice = input('Which recording to analyze? Enter number from above:  ');
+        recording = node.recordings{choice};
+    else
+        recording = node.recordings{1};
+    end
+    
 end
